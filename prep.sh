b8e07905a4042688599a74f35dc2f130c4190f03
Andy Williams <andy@andy.xyz>
2022-03-17 11:47:14 +0000

    Work as expected off macOS sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-env DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`" convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: $DATE\" " ./img/splash.png ./config/includes.binary/isolinux/splash.png
+export DATE=`date "+%Y-%m-%d %H:%M:%S %Z"`
+convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: ${DATE}\" " ./img/splash.png ./config/includes.binary/isolinux/splash.png
 
 lb config
 

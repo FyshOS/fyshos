a88f771b37aded5c9e8e0a7581bf51a977c1d720
Andy Williams <andy@andy.xyz>
2022-03-17 11:41:21 +0000

    Attempt correct font lookup in CI

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DATE=`date "+%Y-%m-%d %H:%M:%S %Z"` convert -family Sans -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: $DATE\" " ./img/splash.png ./config/includes.binary/isolinux/splash.png
+env DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`" convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: $DATE\" " ./img/splash.png ./config/includes.binary/isolinux/splash.png
 
 lb config
 

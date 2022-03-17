0ee2daa529915fb13c51a26af2b6d41a0f5b06f8
Andy Williams <andy@andy.xyz>
2022-03-17 11:52:50 +0000

    Try escaping string

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export DATE=`date "+%Y-%m-%d %H:%M:%S %Z"`
+export DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`"
 convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: ${DATE}\" " ./img/splash.png ./config/includes.binary/isolinux/splash.png
 
 lb config

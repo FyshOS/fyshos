a0086e74b308b950e61bb3280d29c3eef276aa17
Andy Williams <>
2023-01-21 11:06:56 -0800

    First version of a FyshOS prompt

---
+++ config/includes.chroot/etc/skel/.fyshprompt.sh
@@ -0,0 +1,59 @@
+#!/bin/zsh
+
+setopt prompt_subst
+
+function json_value {
+  cat $1 |
+    sed -e 's/[{}]/''/g' |
+    awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |
+    grep "\"$2\"" | awk '{split($0,a,":"); print a[2]}' | sed -e 's/^"//' -e 's/"$//'
+}
+
+function preexec() {
+    timer=${timer:-$SECONDS}
+}
+
+function precmd() {
+  if [ $timer ]; then
+    elapsed=$(($SECONDS - $timer))
+
+    export TIMED="${elapsed}s%{$reset_color%}"
+  else
+    export TIMED=""
+  fi
+  unset timer
+}
+
+
+function clock {
+  PREF=`json_value ~/.config/fyne/com.fyshos.fynedesk/preferences.json clockformatting`
+
+  if [[ "x$PREF" == "x24h" ]]; then
+    FMT="%-H:%M:%S"
+  else
+    FMT="%-I:%M%p"
+  fi
+
+  echo "[$(date +"${FMT}")]"
+}
+
+function status {
+  RET=$(echo $?)
+  if [ $RET -eq 0 ]; then
+    echo '%F{010}'
+  else
+    echo '%F{009}'
+  fi
+}
+
+user=$'%F{027}%n'
+host=$'%F{027}%m'
+workingDir=$'%F{007}%d'
+newLine=$'\n'
+PROMPT=${host}\ ${workingDir}\ ${newLine}${user}" %F{166}><> %1 %f"
+
+dateTime=$'$(status)$TIMED%F{007} $(clock)'
+upLine=$'\e[1A'
+downLine=$'\e[1B'
+RPROMPT=%{${upLine}%}${dateTime}%{${downLine}%}
+

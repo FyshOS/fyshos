6df8a0cf0650d5cb44c037b67584a0e6682ba6d1
Andy Williams <andy@andy.xyz>
2026-01-04 22:00:34 +0000

    Attempting a 32 bit build

--- prep-32.sh
+++ prep-32.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-convert -font open-sans-bold -pointsize 22 -fill white -weight bold -draw "text 405,47 \"x86_64\" " ./img/splash.png splash-tmp.png
 export DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`"
+convert -font open-sans-bold -pointsize 22 -fill white -weight bold -draw "text 405,47 \"i386\" " ./img/splash.png splash-tmp.png
 convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: ${DATE}\" " ./splash-tmp.png ./config/includes.binary/isolinux/splash.png
 rm splash-tmp.png
 

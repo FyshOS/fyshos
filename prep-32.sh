ae53b2ba5d2a336c7a04a887fac9aa737e585578
Andy Williams <andy@andy.xyz>
2026-01-06 21:54:42 +0000

    Adding arm64 distribution too

--- prep-32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`"
-convert -font open-sans-bold -pointsize 22 -fill white -weight bold -draw "text 405,47 \"i386\" " ./img/splash.png splash-tmp.png
-convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: ${DATE}\" " ./splash-tmp.png ./config/includes.binary/isolinux/splash.png
-rm splash-tmp.png
-
-lb config
-
--- prep.sh
+++ prep.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-convert -font open-sans-bold -pointsize 22 -fill white -weight bold -draw "text 405,47 \"x86_64\" " ./img/splash.png splash-tmp.png
+ARCH="${1:-x86_64}"
+
+convert -font open-sans-bold -pointsize 22 -fill white -weight bold -draw "text 405,47 \"$ARCH\" " ./img/splash.png splash-tmp.png
 export DATE="`date "+%Y-%m-%d %H:%M:%S %Z"`"
 convert -font open-sans-bold -pointsize 15 -fill white -weight bold -draw "text 191,107 \"Built: ${DATE}\" " ./splash-tmp.png ./config/includes.binary/isolinux/splash.png
 rm splash-tmp.png
